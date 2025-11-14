e7f99b0fac456609de8d7d27d0e321b6171d9b39
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-14 19:15:47 +0500

    Modularized stylesheets for the 2 major components, CV and Form

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import "./styles/Form.css"
+import "./styles/CV.css"
 import CV from './ui/components/CV'
 import Form from './ui/components/Form'
 
